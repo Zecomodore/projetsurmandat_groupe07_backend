31d9318f0060656828b56d74a6c84741b3505c48
Kilian Lam <91051627+Zecomodore@users.noreply.github.com>
2025-05-13 00:40:09 +0800

    mis à jour du code (ajout de bruno)

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ Route::get('/user', function (Request $request) {
 Route::post('/auth', [AuthController::class, 'auth']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('/interventions', [InterventionController::class, 'get_interventions']);
     Route::post('/interventions', [InterventionController::class, 'create_intervention']);
@@ -48,16 +47,18 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('/lstutilisateur/etat', [ListeUtilisateurController::class, 'get_est_en_intervention_utilisateur']);
     Route::get('/admin/liste_utilisateur/intervention/{id}', [ListeUtilisateurController::class, 'getPersonnesParIntervention']);
     Route::get('/admin/interventions/vehicules/{id}', [ListeVehiculeController::class, 'get_vehicules_par_intervention']);
-    Route::get('/admin/vehicules', [VehiculeController::class, 'get_all_vehicules']);});
-    Route::get('/admin/utilisateurs', [UtilisateurController::class, 'get_all_utilisateurs']);
-    Route::post('/admin/creerutilisateur', [UtilisateurController::class, 'creerUtilisateur']);
-    Route::put('/admin/modifierutilisateur/{id}', [UtilisateurController::class, 'update_utilisateur']);
-    Route::delete('/admin/supprimerutilisateur/{id}', [UtilisateurController::class, 'delete_utilisateur']);
-    Route::post('/admin/creervehicule', [VehiculeController::class, 'creerVehicule']);
-    Route::get('/admin/vehicule/{id}', [VehiculeController::class, 'get_vehicule_admin']);
-    Route::put('/admin/modifiervehicule/{id}', [VehiculeController::class, 'modifierVehicule']);
-    Route::delete('/admin/supprimervehicule/{id}', [VehiculeController::class, 'deleteVehicule']);
-    Route::get('/admin/utilisateur/{id}', [UtilisateurController::class, 'get_utilisateur_admin']);
+    Route::get('/admin/vehicules', [VehiculeController::class, 'get_all_vehicules']);
+});
+
+Route::get('/admin/utilisateurs', [UtilisateurController::class, 'get_all_utilisateurs']);
+Route::post('/admin/creerutilisateur', [UtilisateurController::class, 'creerUtilisateur']);
+Route::put('/admin/modifierutilisateur/{id}', [UtilisateurController::class, 'update_utilisateur']);
+Route::delete('/admin/supprimerutilisateur/{id}', [UtilisateurController::class, 'delete_utilisateur']);
+Route::post('/admin/creervehicule', [VehiculeController::class, 'creerVehicule']);
+Route::get('/admin/vehicule/{id}', [VehiculeController::class, 'get_vehicule_admin']);
+Route::put('/admin/modifiervehicule/{id}', [VehiculeController::class, 'modifierVehicule']);
+Route::delete('/admin/supprimervehicule/{id}', [VehiculeController::class, 'deleteVehicule']);
+Route::get('/admin/utilisateur/{id}', [UtilisateurController::class, 'get_utilisateur_admin']);
 
 Route::post('/envoie', [UtilisateurController::class, 'envoie_email']);
 Route::post('/validation', [UtilisateurController::class, 'code_validation']);
